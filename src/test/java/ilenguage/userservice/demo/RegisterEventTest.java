7bd97c42ced5acae752488534a8c9b3f6cbc8587
lew22 <richardgarmori@gmail.com>
2021-10-27 19:19:18 -0500

    createregister event createExistingUser

--- src/test/java/ilenguage/userservice/demo/RegisterEventTest.java
+++ src/test/java/ilenguage/userservice/demo/RegisterEventTest.java
@@ -1,46 +1,52 @@
 package ilenguage.userservice.demo;
 
-import ILenguage.user.contracts.events.UserRegistered;
+
 import ilenguage.userservice.demo.command.application.dto.request.RegisterUserRequest;
 import ilenguage.userservice.demo.command.application.dto.response.RegisterUserResponse;
 import ilenguage.userservice.demo.command.domain.User;
 import io.cucumber.java.Before;
+import org.axonframework.eventsourcing.eventstore.EventStoreException;
 import org.axonframework.modelling.command.AggregateAnnotationCommandHandler;
 import org.axonframework.test.aggregate.AggregateTestFixture;
 import org.axonframework.test.aggregate.FixtureConfiguration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ilenguage.userservice.demo.command.application.services.UserApplicationService;
 import ILenguage.user.contracts.commands.RegisterUser;
+import ILenguage.user.contracts.events.UserRegistered;
 
 
+import java.time.Instant;
 import java.util.UUID;
 
 public class RegisterEventTest {
     private FixtureConfiguration fixture;
 
-    @Before
+
+    @BeforeEach
     public void setUp() throws Exception{
-        fixture = new AggregateTestFixture<>(RegisterUser.class);
-        AggregateAnnotationCommandHandler commandHandler = new AggregateAnnotationCommandHandler<>(RegisterUser.class, fixture.getRepository());
-        fixture.registerAnnotatedCommandHandler(commandHandler);
+        fixture = new AggregateTestFixture(User.class);
     }
 
     @Test
-    public void testRegister() throws Exception{
+    public void createRegister() throws Exception{
         String userId = UUID.randomUUID().toString();
         RegisterUser registerUser = new RegisterUser(userId,"test1","SWAGtest","75104902");
-        fixture.givenNoPriorActivity()
-                .when(registerUser)
-                .expectEvents(new RegisterUser(userId,"test1","SWAGtest","75104902"));
 
+        fixture.given()
+                .when(new RegisterUser(registerUser.getUserId(),registerUser.getFirstName(),registerUser.getLastName(),registerUser.getDni()))
+                .expectSuccessfulHandlerExecution()
+                .expectEvents(new UserRegistered(registerUser.getUserId(),registerUser.getFirstName(),registerUser.getLastName(),registerUser.getDni(),));
     }
-//    @Test
-//    public void testFirstFixture() {
-//        String userId = UUID.randomUUID().toString();
-//        fixture.given(new RegisterUser(userId,"test1","SWAGtest","75104902"))
-//                .when(new RegisterUserResponse(registerUser.getUserId(),registerUser.getFirstName(),registerUser.getLastName(),registerUser.getDni()))
-//                .expectSuccessfulHandlerExecution()
-//                .expectEvents(new RegisterUser(userId,"test1","SWAGtest","75104902"));
-//    }
+
+    @Test
+    public void createExistingUser() {
+        String userId = UUID.randomUUID().toString();
+        RegisterUser registerUser = new RegisterUser(userId,"test1","SWAGtest","75104902");
+
+        fixture.given(new UserRegistered(registerUser.getUserId(),registerUser.getFirstName(),registerUser.getLastName(),registerUser.getDni(),))
+                .when(new RegisterUser(userId,"test1","SWAGtest","75104902"))
+                .expectException(EventStoreException.class);
+    }
 
 }
